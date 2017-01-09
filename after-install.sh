f814ceab169c9a23ef7b2369bbf9a8b8229cb442
Brian Yu <brianyu28@gmail.com>
2017-01-09 15:08:55 -0500

    Add permissions to after-install

--- after-install.sh
+++ after-install.sh
@@ -4,6 +4,11 @@
 apt-get install python3-pexpect python3-termcolor
 pip3 install getch
 
+# ensure dependencies are readable
+chmod a+r /usr/local/lib/python3.4/dist-packages/getch.cpython-34m.so
+chmod a+r /usr/local/lib/python3.4/dist-packages/termcolor.py
+chmod -R a+rX /usr/local/lib/python3.4/dist-packages/pexpect/
+
 # ensure submit50 is executable
 chmod -R a+rX /opt/cs50/submit50
 chmod -R a+x /opt/cs50/submit50/bin/*
