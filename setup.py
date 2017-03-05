912beca53e1fce0e87fb2697fd5932a2156979c6
Kareem Zidane <kzidane@cs50.harvard.edu>
2017-03-05 15:49:53 +0200

    requires requests and reads version programmatically

--- setup.py
+++ setup.py
@@ -14,10 +14,10 @@ setup(
         "Topic :: Utilities"
     ],
     description="This is submit50, with which you can submit solutions to problems for CS50.",
-    install_requires=["getch", "pexpect", "termcolor"],
+    install_requires=["getch", "pexpect", "requests", "termcolor"],
     keywords=["submit", "submit50"],
     name="submit50",
     scripts=["submit50"],
     url="https://github.com/cs50/submit50",
-    version="2.1.4"
+    version="2.1.5"
 )
