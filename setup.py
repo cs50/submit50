9ee88fd3c179935e392c81094541c4bc3a8640be
Chad Sharp <chad@cs50.harvard.edu>
2018-07-15 12:16:39 -0400

    remove InstallWithCompile

--- setup.py
+++ setup.py
@@ -1,20 +1,5 @@
 from setuptools import setup
 
-import glob
-import os
-import subprocess
-
-def create_mo_files():
-    po_pattern = "locale/*/LC_MESSAGES/*.po"
-    for prefix in glob.glob(po_pattern):
-        for _,_,files in os.walk(prefix):
-            for file in files:
-                po_file = Path(prefix) / po_file
-                mo_file = po_file.parent / po_file.stem + ".mo"
-                subprocess.call(["msgfmt", "-o", mo_file, po_file])
-
-create_mo_files()
-
 setup(
     author="CS50",
     author_email="sysadmins@cs50.harvard.edu",
@@ -24,16 +9,17 @@ setup(
         "Topic :: Education",
         "Topic :: Utilities"
     ],
-    description="This is submit50, with which you can submit solutions to \
-problems for CS50.",
-    install_requires=["requests", "termcolor", "push50"],
+    cmdclass=cmdclass,
+    message_extractors = {
+        'submit50': [('**.py', 'python', None),],
+    },
+    description="This is submit50, CS50's internal library for using GitHub as data storage.",
+    install_requires=["babel", "push50", "requests", "termcolor"],
     keywords=["submit", "submit50"],
     name="submit50",
+    python_requires=">= 3.6",
     packages=["submit50"],
-    entry_points={
-        "console_scripts": ["submit50=submit50.__main__:main"]
-    },
     url="https://github.com/cs50/submit50",
-    version="3.0.0",
-    include_package_data=True,
+    version="1.0.0",
+    include_package_data=True
 )
