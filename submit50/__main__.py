0e590563b8623c3ea4f307ef871b646918482f31
Chad Sharp <chad@cs50.harvard.edu>
2018-08-14 14:44:22 -0400

    added comments
    Resolves: #170

--- submit50/__main__.py
+++ submit50/__main__.py
@@ -137,12 +137,14 @@ def main():
     excepthook.verbose = args.verbose
     if args.verbose:
         logging.basicConfig(level="INFO")
+        # Disable progress bar so it doesn't interfere with log
         lib50.ProgressBar.DISABLED = True
 
     check_announcements()
     check_version()
 
     lib50.push("submit50", args.slug, prompt=prompt)
+
 
 if __name__ == "__main__":
     main()
