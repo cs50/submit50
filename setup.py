474461a891dffa2cc3e13f8be8da63524cb63ec2
Chad Sharp <chad@cs50.harvard.edu>
2019-07-29 13:00:35 -0400

    lib50 version++

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
         'submit50': [('**.py', 'python', None),],
     },
     description="This is submit50, with which you can submit solutions to problems for CS50.",
-    install_requires=["lib50>=1.1.4", "requests>=2.19", "termcolor>=1.1"],
+    install_requires=["lib50>=1.1.5", "requests>=2.19", "termcolor>=1.1"],
     keywords=["submit", "submit50"],
     name="submit50",
     python_requires=">=3.6",
