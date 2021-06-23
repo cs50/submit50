2d12796437c070982aa7d55eb3caab2cb464c7b3
Kareem Zidane <kzidane@cs50.harvard.edu>
2021-06-23 19:55:55 -0400

    Use context managers for cloning

--- src/submit50/git.py
+++ src/submit50/git.py
@@ -1,14 +1,20 @@
+import contextlib
 import os
 import subprocess
+import tempfile
 
 from os.path import join
 from tempfile import mkdtemp
 
 from .assignment import assignment_name_from_identifier, assignment_name_from_remote
 
-class GitRepo:
-    def __init__(self):
-        pass
+
+# TODO log outputs in verbose mode
+
+class Git:
+    def __init__(self, identifier, git_host=os.getenv('SUBMIT50_GIT_HOST', 'https://github.com/')):
+        self.git_host = git_host
+        self.identifier = identifier
 
     @staticmethod
     def assert_git_installed():
@@ -20,49 +26,50 @@ class GitRepo:
         except FileNotFoundError:
             raise RuntimeError('It looks like git is not installed. Please install git then try again.')
 
+    @contextlib.contextmanager
+    def clone_assignment_template(self):
+        """
+        Clones assignment template into a temporary directory.
 
+        :param identifer: The name of the student's copy of the assignment (E.g.,
+            org/assignment-username)
+        """
+        assignment_name = assignment_name_from_identifier(self.identifier)
+        remote = join(self.git_host, assignment_name)
+        with tempfile.TemporaryDirectory() as temp_dir:
+            try:
+                subprocess.check_output(
+                    ['git', 'clone', '--depth', '1', '--quiet', remote, temp_dir]
+                )
+            except subprocess.CalledProcessError:
+                raise RuntimeError(f'Failed to clone "{remote}".')
+            yield temp_dir
 
-GIT_HOST = os.getenv('SUBMIT50_GIT_HOST', 'https://github.com')
+    @contextlib.contextmanager
+    def clone_student_assignment_bare(self):
+        remote = join(self.git_host, self.identifier)
+        with tempfile.TemporaryDirectory() as git_dir:
+            try:
+                subprocess.check_output(['git', 'clone', '--bare', '--quiet', remote, git_dir])
+            except subprocess.CalledProcessError:
+                assignment_name = assignment_name_from_remote(remote)
+                raise RuntimeError(
+                    f'Failed to clone "{remote}". Did you accept assignment "{assignment_name}"?')
+            self.git_dir = git_dir
+            yield git_dir
 
-# TODO log outputs in verbose mode
+    def enable_credential_cache(self):
+        """
+        Configures git credential cache helper if no credential helper is configured. The cache
+        helper stores credentials in memory for 15 minutes after the user provides them.
+        """
+        if not self._git(['config', 'credential.helper']):
+            self._git(['config', 'credential.helper', 'cache'])
 
-def clone_assignment_template(identifier):
-    """
-    Clones assignment template into a temporary directory.
+    def _git(self, args):
+        return subprocess.check_output(['git'] + args, env=self.env)
 
-    :param identifer: The name of the student's copy of the assignment (E.g.,
-        org/assignment-username)
-    """
-    assignment_name = assignment_name_from_identifier(identifier)
-    remote = join(GIT_HOST, assignment_name)
-    assignment_temp_path = _clone_assignment_template_in_temp_dir(remote)
-    return assignment_temp_path
-
-def _clone_assignment_template_in_temp_dir(remote):
-    temp_path = mkdtemp()
-    try:
-        subprocess.check_output(['git', 'clone', '--depth', '1', '--quiet', remote, temp_path])
-    except subprocess.CalledProcessError:
-        raise RuntimeError(f'Failed to clone "{remote}".')
-    return temp_path
-
-def clone_student_assignment_bare(identifier):
-    remote = join(GIT_HOST, identifier)
-    assignment_bare_path = _clone_student_assignment_bare_in_temp_dir(remote)
-    return assignment_bare_path
-
-def _clone_student_assignment_bare_in_temp_dir(remote):
-    temp_path = mkdtemp()
-    try:
-        subprocess.check_output(['git', 'clone', '--bare', '--quiet', remote, temp_path])
-    except subprocess.CalledProcessError:
-        assignment_name = assignment_name_from_remote(remote)
-        raise RuntimeError(
-            f'Failed to clone "{remote}". Did you accept assignment "{assignment_name}"?')
-    return temp_path
-
-def clone_add_commit_push(identifier):
-    git_dir = clone_student_assignment_bare(identifier)
+def add_commit_push(git_dir):
     try:
         env = os.environ.copy()
         env['GIT_DIR'] = git_dir
--- src/submit50/submit.py
+++ src/submit50/submit.py
@@ -3,8 +3,11 @@ from .git import *
 
 
 def submit(identifier):
-    GitRepo.assert_git_installed()
+    Git.assert_git_installed()
     assignment = Assignment(identifier)
-    assignment_template_dir = clone_assignment_template(identifier)
-    copy_dotfiles_from(assignment_template_dir)
-    clone_add_commit_push(identifier)
+    assignment_template_client = Git(identifier)
+    with assignment_template_client.clone_assignment_template() as assignment_template_dir:
+        copy_dotfiles_from(assignment_template_dir)
+        student_assignment_client = Git(identifier)
+        with student_assignment_client.clone_student_assignment_bare() as student_assignment_git_dir:
+            add_commit_push(student_assignment_git_dir)
