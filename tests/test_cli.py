4c81f95ef352556c50eddfe407e831032cc5a01c
Kareem Zidane <kzidane@cs50.harvard.edu>
2021-07-03 06:20:20 -0400

    Use importlib instead of imp

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,13 +1,22 @@
-import imp
+import importlib
 import os
 import sys
 import unittest
 
+from importlib.util import spec_from_loader, module_from_spec
+from importlib.machinery import SourceFileLoader
 from unittest.mock import patch
 
 
 tests_dir = os.path.dirname(os.path.realpath(__file__))
-cli = imp.load_source('cli', os.path.join(tests_dir, '..', 'bin', 'submit50'))
+
+spec = spec_from_loader(
+    'cli',
+    SourceFileLoader('cli', os.path.join(tests_dir, '..', 'bin', 'submit50'))
+)
+cli = module_from_spec(spec)
+sys.modules['cli'] = cli
+spec.loader.exec_module(cli)
 
 @patch('cli.configure_logging')
 @patch('cli.submit')
