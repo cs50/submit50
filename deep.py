b7d907ad0c2aa0d375cc1fdc3e5656c6cb399200
zainabmossadiq0505 <107940230+zainabmossadiq0505@users.noreply.github.com>
2022-06-24 12:41:14 -0700

    Create deep.py

---
+++ deep.py
@@ -0,0 +1,6 @@
+answer= input ("What is the Answer to the Greatest of Life, the Universe, and Everything?" ).strip().title()
+if answer=='42'or answer=='Forty Two' or answer=='Forty-Two':
+    print("Yes")
+else:
+    print("No")
+  
