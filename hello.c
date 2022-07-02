828e0421bfb930953ea5bfdba4724e69b05a051e
Hfaraj18 <103384144+Hfaraj18@users.noreply.github.com>
2022-07-02 18:56:27 +0300

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include<cs50.h>
+int main (void)
+{
+
+    printf("hello,world\n");
+
+
+}
---
+++ name.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <cs50.h>
+int main (void)
+{
+
+    string name = get_string("whats your name?");
+    printf("hello,%s\n",name);
+
+}
